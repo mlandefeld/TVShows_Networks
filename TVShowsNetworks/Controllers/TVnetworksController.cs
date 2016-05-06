1e6057600611c3ff0943dc2616482d4eb16d0f63
Margaret Landefeld <mlandefeld@users.noreply.github.com>
2016-05-05 20:13:45 -0400

    first commit

---
+++ TVShowsNetworks/Controllers/TVnetworksController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TVShowsNetworks.Models;
+
+namespace TVShowsNetworks.Controllers
+{
+    public class TVnetworksController : Controller
+    {
+        private TVShowsNetworksEntities db = new TVShowsNetworksEntities();
+
+        // GET: TVnetworks
+        public ActionResult Index()
+        {
+            return View(db.TVnetworks.ToList());
+        }
+
+        // GET: TVnetworks/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TVnetwork tVnetwork = db.TVnetworks.Find(id);
+            if (tVnetwork == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tVnetwork);
+        }
+
+        // GET: TVnetworks/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TVnetworks/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "NetworkID,Name,Logo")] TVnetwork tVnetwork)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TVnetworks.Add(tVnetwork);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tVnetwork);
+        }
+
+        // GET: TVnetworks/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TVnetwork tVnetwork = db.TVnetworks.Find(id);
+            if (tVnetwork == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tVnetwork);
+        }
+
+        // POST: TVnetworks/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "NetworkID,Name,Logo")] TVnetwork tVnetwork)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tVnetwork).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tVnetwork);
+        }
+
+        // GET: TVnetworks/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TVnetwork tVnetwork = db.TVnetworks.Find(id);
+            if (tVnetwork == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tVnetwork);
+        }
+
+        // POST: TVnetworks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TVnetwork tVnetwork = db.TVnetworks.Find(id);
+            db.TVnetworks.Remove(tVnetwork);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TVShowsNetworks/Controllers/TVshowsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TVShowsNetworks.Models;
+
+namespace TVShowsNetworks.Controllers
+{
+    public class TVshowsController : Controller
+    {
+        private TVShowsNetworksEntities db = new TVShowsNetworksEntities();
+
+        // GET: TVshows
+        public ActionResult Index()
+        {
+            var tVshows = db.TVshows.Include(t => t.TVnetwork);
+            return View(tVshows.ToList());
+        }
+
+        // GET: TVshows/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TVshow tVshow = db.TVshows.Find(id);
+            if (tVshow == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tVshow);
+        }
+
+        // GET: TVshows/Create
+        public ActionResult Create()
+        {
+            ViewBag.NetworkID = new SelectList(db.TVnetworks, "NetworkID", "Name");
+            return View();
+        }
+
+        // POST: TVshows/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ShowID,NetworkID,Title,Length,Genre")] TVshow tVshow)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TVshows.Add(tVshow);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.NetworkID = new SelectList(db.TVnetworks, "NetworkID", "Name", tVshow.NetworkID);
+            return View(tVshow);
+        }
+
+        // GET: TVshows/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TVshow tVshow = db.TVshows.Find(id);
+            if (tVshow == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.NetworkID = new SelectList(db.TVnetworks, "NetworkID", "Name", tVshow.NetworkID);
+            return View(tVshow);
+        }
+
+        // POST: TVshows/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ShowID,NetworkID,Title,Length,Genre")] TVshow tVshow)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tVshow).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.NetworkID = new SelectList(db.TVnetworks, "NetworkID", "Name", tVshow.NetworkID);
+            return View(tVshow);
+        }
+
+        // GET: TVshows/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TVshow tVshow = db.TVshows.Find(id);
+            if (tVshow == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tVshow);
+        }
+
+        // POST: TVshows/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TVshow tVshow = db.TVshows.Find(id);
+            db.TVshows.Remove(tVshow);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
